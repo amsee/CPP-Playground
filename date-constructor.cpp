c07b46adc11fe1dbb5ca177997ae9d79dc27ef35
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-02-12 16:03:43 -0500

    Construct Assignment 4

--- date-constructor.cpp
+++ date-constructor.cpp
@@ -11,18 +11,41 @@ class Date
         int day;
 // todo - int year 
         int year;
-}
+
 // todo date (int, int, int) // ! only accept if valid numbers
     public:
-        Date()
+        Date(void)
         {
             month = 0;
             day = 0;
-            yaer = 0;
+            year = 0;
         }
 // todo getDates const 
-    Date(int m, int d, int y) const
-
+    Date(int m, int d, int y)
+    {
+        month = m;
+        day = d;
+        year = y;
+    }
 // todo display all entered dates
+    void displayInfo(void) const
+    {
+        cout << "The month is: " << month << endl;
+        cout << "The day is: " << day << endl;      
+        cout << "The year is: " << year << endl;
+    }
+};      
 // todo dynamically allocate dates
+int main(void)
+{
+    Date mm;
+    Date dd;
+    Date yy;
+
+    mm.displayInfo();
+    dd.displayInfo();
+    yy.displayInfo();
+
+    return 0;
+}
 // todo - ask user how many dates will be entered 
