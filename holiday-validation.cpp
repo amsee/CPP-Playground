6675628b4ad9f2eba822668d45009f60d66d3333
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-02-15 20:14:51 -0500

    Validation - Assignment 2

---
+++ holiday-validation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Holiday
+{
+    private:
+        int thanksgiving;
+        int christmas;
+        int valentines;
+    public:
+    Holiday(void)
+    {
+        thanksgiving = 0;
+        christmas = 0;
+        valentines = 0;
+    }
+    Holiday(int t, int c, int v)
+    {
+        if (thanksgiving == 8)
+        {
+            thanksgiving = t;
+        }
+        if (christmas == 25)
+        {
+            christmas = c;
+        }
+        if (valentines == 14)
+        {
+            valentines = v;
+        }
+    }
+    void DisplayInformation(void)
+    {
+        cout << "What day is Thanksgiving on? " << thanksgiving << endl;
+        cout << "What day is Christmas on? " << christmas << endl;
+        cout << "What day is Valentines on? " << valentines << endl;
+    }
+};
+int main(void)
+{
+    Holiday *days;
+    Holiday h(8, 25, 14);
+
+    cout << "Special" << endl;
+    
+    h.DisplayInformation();
+    return 0;
+}
