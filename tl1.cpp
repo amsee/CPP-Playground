695e0dde36adb9e86bb342ea2d740ac74af60e68
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-02-22 20:08:47 -0500

    Deleted Executables

--- tl1.cpp
+++ tl1.cpp
@@ -1,45 +1,16 @@
-// todo - Book (check if there are more than 10 pages for the book to be valid)
+// main.cpp
 #include <iostream>
+#include "Grade.h"
 using namespace std;
+using namespace Amy;
 
-class Book
-{
-    private:
-        int pages;
-    public:
-    Book(void)
-    {
-        pages = 0;
-    }
-    Book(int p)
-    {
-        bool valid = p == 10 || p == 9 || p == 8 || p == 7 || p == 6 || p == 5 || p == 4 || p == 3 || p == 2 || p == 1;
-        if (valid)
-        {
-            pages = p;
-            ValidMessage();
-        }
-        else
-        {
-            *this = Book(); // * call?
-            InvalidMessage();
-        }
-    }
-    void ValidMessage(void) const
-    {
-        cout << "This is the right book." << endl;
-    }
-    void InvalidMessage(void) const
-    {
-        cout << "This is the wrong book." << endl;
-    }
-};
 int main(void)
 {
-    int pages;
-    cout << "Enter # of Pages: " << endl;
-    cin >> pages;
+    char letter;
+    cout << "Enter Grade: " << endl;
+    cin >> letter;
 
-    Book b(pages);
+    Grade g(letter);
+
     return 0;
 }
