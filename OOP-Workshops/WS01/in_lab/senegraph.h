b82854c74ccc7be887bf1901011d175cbaf2858b
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-02-28 09:25:03 -0500

    Commit header

---
+++ OOP-Workshops/WS01/in_lab/senegraph.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "tools.h"
+#include "graph.h"
+#include "senegrpah.cpp" 
+
+namespace Senegraph
+{
+    int main
+    [
+         getSamples(samples, noOfSamples);
+         printGraph(samples, noOfSamples);
+    ]
+}
